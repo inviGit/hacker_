163f663bb989f10379f7df074e5de45374a97b9f
Rishabh <rshbhrao20@gmail,com>
2021-04-21 03:27:09 +0530

    updated queries

--- routes/hacker.js
+++ routes/hacker.js
@@ -3,26 +3,23 @@ var router = express.Router();
 var connection = require("../lib/db");
 
 router.get("/byid/:id", function (req, res, next) {
-    const id = req.params['id']
-    const query = "SELECT * FROM user_hackers where id = $1"
-    const values =[id]
-    connection.query(query, values, function (err, rows) {
-      if (err) throw err;
-      // if user not found
-      if (rows.length <= 0) {
-        res.send({ message: "failed" });
-      } else {
-        res.send(rows[0]);
-      }
-    });
+  const id = req.params["id"];
+  const query = "SELECT * FROM user_hackers where id = $1";
+  const values = [id];
+  connection.query(query, values, (err, rows) => {
+    if (err)  {
+      res.send({ status: 0, message: err });
+    } else {
+      res.send(rows[0]);
+    }
   });
+});
 
 router.get("/all", function (req, res, next) {
-  connection.query("SELECT * FROM user_hackers", [], function (err, rows) {
-    if (err) throw err;
-    // if user not found
-    if (rows.length <= 0) {
-      res.send({ message: "failed" });
+  connection.query("SELECT * FROM user_hackers", (err, rows) => {
+    if (err) {
+      res.send({ status: 0, message: err });
+      console.log(err.stack);
     } else {
       res.send(rows);
     }
@@ -30,15 +27,16 @@ router.get("/all", function (req, res, next) {
 });
 
 router.get("/ranks", function (req, res, next) {
-  connection.query("SELECT username, name, (solutions_submitted - challenges_solved) as s FROM user_hackers where challenges_solved>10 order by s desc limit 3", [], function (err, rows) {
-    if (err) throw err;
-    // if user not found
-    if (rows.length <= 0) {
-      res.send({ message: "failed to get details" });
-    } else {
-      res.send(rows);
+  connection.query(
+    "SELECT username, name, (solutions_submitted - challenges_solved) as s FROM user_hackers where challenges_solved>10 order by s desc limit 3",
+    function (err, rows) {
+      if (err) {
+        res.send({ status: 0, message: err });
+      } else {
+        res.send(rows);
+      }
     }
-  });
+  );
 });
 
 module.exports = router;
